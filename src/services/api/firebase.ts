ee7c0a392a3101c000b8eaa88d32eb386da5a1c0
Shems45 <shemsedine.boughaleb@student.ehb.be>
2025-12-02 11:41:02 +0100

    firebase support added

--- src/services/api/firebase.ts
+++ src/services/api/firebase.ts
@@ -1,17 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, get, set, update, remove, push } from "firebase/database";
 
+// Firebase config van jouw project
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyCyWMNSFXZJfq40U1PomrRA4D_S_HYOlKA",
+  authDomain: "habithero-73d98.firebaseapp.com",
+  databaseURL: "https://habithero-73d98-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "habithero-73d98",
+  storageBucket: "habithero-73d98.appspot.com",
+  messagingSenderId: "63808260264",
+  appId: "1:63808260264:web:fcbfe863fc8011ab2a1a9d"
 };
 
+// Firebase initialiseren
 export const app = initializeApp(firebaseConfig);
+
+// Realtime Database initialiseren
 export const db = getDatabase(app);
 
+// Exporteer database functies
 export { ref, get, set, update, remove, push };
