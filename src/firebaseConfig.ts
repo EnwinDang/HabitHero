ca988d3a3b13133579a08442d750c5e6806989bb
Shems45 <shemsedine.boughaleb@student.ehb.be>
2025-12-13 20:16:07 +0100

    update support voor vite en api key verbergen met een env

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,17 +3,14 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyWMNSFXZJfq40U1PomrRA4D_S_HYOlKA",
-  authDomain: "habithero-73d98.firebaseapp.com",
-  databaseURL: "https://habithero-73d98-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "habithero-73d98",
-  storageBucket: "habithero-73d98.firebasestorage.app",
-  messagingSenderId: "63808260264",
-  appId: "1:63808260264:web:fcbfe863fc8011ab2a1a9d",
-  measurementId: "G-WVWY29BKQW"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const db = getDatabase(app);
